546ceed505e916a48243d1de413003bce8c3f556
DevJohanR <desarrollador7777@gmail.com>
2024-04-10 13:55:57 -0500

    segundo upload integrador - se borra el archivo data del app.jsx y se busca implementar USESTATE

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,19 @@
 import './App.css';
-import Card from './components/Card.jsx';
-import Cards from './components/Cards.jsx';
-import SearchBar from './components/SearchBar.jsx';
-import characters, { Rick } from './data.js';
+import Card from './components/Card/Card.jsx';
+import Cards from './components/Cards/Cards.jsx';
+
+import Navbar from './components/Navbar/Navbar.jsx';
+import { useState } from 'react';
+
 
 function App() {
    return (
       <div className='App'>
-         <SearchBar onSearch={(characterID) => window.alert(characterID)} />
+         <Navbar onSearch={(characterID) => window.alert(characterID)} />
+        
          <Cards characters={characters} />
          <hr/>
-         <Card
-            id={Rick.id}
-            name={Rick.name}
-            status={Rick.status}
-            species={Rick.species}
-            gender={Rick.gender}
-            origin={Rick.origin.name}
-            image={Rick.image}
-            onClose={() => window.alert('Emulamos que se cierra la card')}
-         />
+        
       </div>
    );
 }
