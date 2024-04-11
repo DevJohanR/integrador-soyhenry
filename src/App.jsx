1d7965632212966650a954ae4fc2cb2ddc4389f6
DevJohanR <desarrollador7777@gmail.com>
2024-04-11 13:55:48 -0500

    respaldo integrador 11-abril

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,41 @@
 import './App.css';
-import Card from './components/Card/Card.jsx';
 import Cards from './components/Cards/Cards.jsx';
-
 import Navbar from './components/Navbar/Navbar.jsx';
 import { useState } from 'react';
+import axios from 'axios';
 
 
 function App() {
+
+   const [characters, setCharacters] = useState([])//[estado local, funcion que me cambia el estado local]
+
+// [characters, setCharacters] = [estado, funcion]
+//characters = estado
+//setCharacters = funcion
+
+const example = {
+   id: 1,
+   name: 'Rick Sanchez',
+   status: 'Alive',
+   species: 'Human',
+   gender: 'Male',
+   origin: {
+      name: 'Earth (C-137)',
+      url: 'https://rickandmortyapi.com/api/location/1',
+   },
+   image: 'https://rickandmortyapi.com/api/character/avatar/1.jpeg',
+};
+
+
+function onSearch (id){
+   setCharacters([...characters, example]) 
+}
+
+
+
    return (
       <div className='App'>
-         <Navbar onSearch={(characterID) => window.alert(characterID)} />
+         <Navbar onSearch={onSearch} />
         
          <Cards characters={characters} />
          <hr/>
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,18 @@
+import { useState } from "react";
+
 export default function SearchBar(props) {
+
+   const [id, setId] = useState('');
+
+   function handleChange(event){
+setId (event.target.value);
+console.log(event.target.value)
+   }
+
    return (
       <div>
-         <input type='search' />
-         <button onClick={()=>props.onSearch('Hola soy un id')}>Agregar</button>
+         <input type='search' value={id} onChange={handleChange}/>
+         <button onClick={props.onSearch}>Agregar</button>
       </div>
    );
 }
